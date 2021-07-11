16c71ccf355862f9c61f2a4c93f688780f4dd788
josephparsival <63027285+josephparsival@users.noreply.github.com>
2021-07-11 21:03:38 +0000

    Update Initial_Install.sh

--- Initial_Install.sh
+++ Initial_Install.sh
@@ -2,4 +2,4 @@
 
 
 #install ngrok
-wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip && unzip ngrok.zip && ./ngrok authtoken 1aHl4ETlBXLXkcu1WTGYYmFvC3L_4nhpct7d2kKpU6rP8UAfz
+wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip && unzip ngrok*.zip && ./ngrok authtoken 1aHl4ETlBXLXkcu1WTGYYmFvC3L_4nhpct7d2kKpU6rP8UAfz
