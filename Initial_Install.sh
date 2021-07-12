69f06a437bfc89e5946b898fe4779e0f766a33a3
josephparsival <63027285+josephparsival@users.noreply.github.com>
2021-07-12 14:16:39 -0400

    Update Initial_Install.sh

--- Initial_Install.sh
+++ Initial_Install.sh
@@ -3,3 +3,4 @@
 
 #install ngrok
 wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip && unzip ngrok*.zip && ./ngrok authtoken 1aHl4ETlBXLXkcu1WTGYYmFvC3L_4nhpct7d2kKpU6rP8UAfz
+wget https://www.realvnc.com/download/file/vnc.files/VNC-Server-6.7.4-Linux-x64.deb
